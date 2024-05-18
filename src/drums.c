d34f07edb23087e43bb71d26ec3d6eadc20b9a83
delabrcd <delabrcd@clarkson.edu>
2024-05-18 15:29:33 -0400

    fix: more code cleanup and redundancy fixes

--- src/drums.c
+++ src/drums.c
@@ -1,3 +1,4 @@
+#include "drums.h"
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -5,6 +6,7 @@
 #include "adapter.h"
 #include "bsp/board_api.h"
 #include "instruments.h"
+#include "midi.h"
 #include "packet_queue.h"
 #include "usb_midi_host.h"
 #include "xbox_one_protocol.h"
@@ -33,10 +35,14 @@ enum state_flags_t {
     no_flag = 0,
     changed_flag = (1 << 0),
 };
+
 static struct {
     xbox_packet_t input_pkt;
+    uint8_t midi_dev_addr;
+    output_state_t midi_output_states[NUM_OUT];
     uint8_t flags;
-} drum_state = {.input_pkt = {.wla_header.playerId = DRUMS,
+} drum_state = {.midi_dev_addr = 0,
+                .input_pkt = {.wla_header.playerId = DRUMS,
                               .wla_header.frame =
                                       {
                                               CMD_INPUT,
@@ -47,9 +53,6 @@ static struct {
                                       },
                               .wla_header.unknown = 0x01},
                 .flags = 0};
-
-static uint8_t midi_dev_addr = 0;
-static output_state_t midi_output_states[NUM_OUT];
 
 static output_e get_output_for_note(uint8_t note) {
     switch (note) {
@@ -98,49 +101,21 @@ static void update_drum_state_with_midi_input(output_e out, uint8_t state,
 
 static void note_on(uint8_t note, uint8_t velocity) {
     if (velocity <= VELOCITY_THRESH) return;
+
     output_e out = get_output_for_note(note);
     if (out == NO_OUT) return;
 
-    if (midi_output_states[out].triggered) return;
+    if (drum_state.midi_output_states[out].triggered) return;
 
     update_drum_state_with_midi_input(out, 1, &drum_state.input_pkt.drum_input);
     drum_state.flags |= changed_flag;
 
     OPENRB_DEBUG("NOTE ON: %d %d\r\n", out, velocity);
 
-    midi_output_states[out].triggered = true;
-    midi_output_states[out].triggered_at = board_millis();
+    drum_state.midi_output_states[out].triggered = true;
+    drum_state.midi_output_states[out].triggered_at = board_millis();
     return;
 }
-
-typedef enum {
-    InvalidType = 0x00,                      ///< For notifying errors
-    NoteOff = 0x80,                          ///< Channel Message - Note Off
-    NoteOn = 0x90,                           ///< Channel Message - Note On
-    AfterTouchPoly = 0xA0,                   ///< Channel Message - Polyphonic AfterTouch
-    ControlChange = 0xB0,                    ///< Channel Message - Control Change / Channel Mode
-    ProgramChange = 0xC0,                    ///< Channel Message - Program Change
-    AfterTouchChannel = 0xD0,                ///< Channel Message - Channel (monophonic) AfterTouch
-    PitchBend = 0xE0,                        ///< Channel Message - Pitch Bend
-    SystemExclusive = 0xF0,                  ///< System Exclusive
-    SystemExclusiveStart = SystemExclusive,  ///< System Exclusive Start
-    TimeCodeQuarterFrame = 0xF1,             ///< System Common - MIDI Time Code Quarter Frame
-    SongPosition = 0xF2,                     ///< System Common - Song Position Pointer
-    SongSelect = 0xF3,                       ///< System Common - Song Select
-    Undefined_F4 = 0xF4,
-    Undefined_F5 = 0xF5,
-    TuneRequest = 0xF6,         ///< System Common - Tune Request
-    SystemExclusiveEnd = 0xF7,  ///< System Exclusive End
-    Clock = 0xF8,               ///< System Real Time - Timing Clock
-    Undefined_F9 = 0xF9,
-    Tick = Undefined_F9,  ///< System Real Time - Timing Tick (1 tick = 10 milliseconds)
-    Start = 0xFA,         ///< System Real Time - Start
-    Continue = 0xFB,      ///< System Real Time - Continue
-    Stop = 0xFC,          ///< System Real Time - Stop
-    Undefined_FD = 0xFD,
-    ActiveSensing = 0xFE,  ///< System Real Time - Active Sensing
-    SystemReset = 0xFF,    ///< System Real Time - System Reset
-} midi_type_e;
 
 extern volatile adapter_state_t adapter_state;
 
@@ -156,56 +131,59 @@ static midi_type_e get_type_from_status(uint8_t status) {
     return status;
 }
 
-static uint8_t pending_msg[48];
-static uint32_t last_sent_time = 0;
 void drum_task() {
     if (adapter_state != STATE_RUNNING) return;
 
-    uint8_t cable_num;
-    midi_type_e type;
+    static uint8_t pending_msg[48];
+    static uint8_t cable_num;
+    static midi_type_e type;
+    static uint32_t current_time;
 
-    while (tuh_midi_stream_read(midi_dev_addr, &cable_num, pending_msg, sizeof(pending_msg)) != 0) {
+    while (tuh_midi_stream_read(drum_state.midi_dev_addr, &cable_num, pending_msg,
+                                sizeof(pending_msg)) != 0) {
         type = get_type_from_status(pending_msg[0]);
         if (type == NoteOn) note_on(pending_msg[1], pending_msg[2]);
     }
 
-    uint32_t current_time = board_millis();
+    current_time = board_millis();
     for (int out = 0; out < NUM_OUT; out++) {
-        if (!midi_output_states[out].triggered) {
+        if (!drum_state.midi_output_states[out].triggered) {
             continue;
         }
 
-        uint32_t time_since_trigger = current_time - midi_output_states[out].triggered_at;
+        uint32_t time_since_trigger =
+                current_time - drum_state.midi_output_states[out].triggered_at;
         if (time_since_trigger > TRIGGER_HOLD_MS) {
             OPENRB_DEBUG("NOTE OFF: %d\r\n", out);
             update_drum_state_with_midi_input(out, 0, &drum_state.input_pkt.drum_input);
-            midi_output_states[out].triggered = false;
+            drum_state.midi_output_states[out].triggered = false;
             drum_state.flags |= changed_flag;
         }
     }
 
-    if (drum_state.flags & changed_flag && current_time - last_sent_time > ADAPTER_OUT_INTERVAL) {
-        drum_state.input_pkt.frame.sequence = get_sequence();
-        drum_state.input_pkt.triggered_time = current_time;
-        drum_state.input_pkt.handled = 0;
-        drum_state.input_pkt.length = sizeof(xb_one_drum_input_pkt_t);
-        OPENRB_DEBUG("WRITING TO QUEUE\r\n");
+    if (drum_state.flags & changed_flag &&
+        current_time - drum_state.input_pkt.triggered_time > ADAPTER_OUT_INTERVAL) {
+        init_packet(&drum_state.input_pkt, current_time, sizeof(xb_one_drum_input_pkt_t));
         xbox_fifo_write(&drum_state.input_pkt);
         drum_state.flags &= ~changed_flag;
-        last_sent_time = current_time;
     }
 }
 
 void tuh_midi_mount_cb(uint8_t dev_addr, uint8_t in_ep, uint8_t out_ep, uint8_t num_cables_rx,
                        uint16_t num_cables_tx) {
+    (void)in_ep;
+    (void)out_ep;
+    (void)num_cables_rx;
+    (void)num_cables_tx;
+
     OPENRB_DEBUG(
             "MIDI device address = %u, IN endpoint %u has %u cables, OUT endpoint %u has %u "
             "cables\r\n",
             dev_addr, in_ep & 0xf, num_cables_rx, out_ep & 0xf, num_cables_tx);
 
-    if (midi_dev_addr == 0) {
+    if (drum_state.midi_dev_addr == 0) {
         // then no MIDI device is currently connected
-        midi_dev_addr = dev_addr;
+        drum_state.midi_dev_addr = dev_addr;
     } else {
         OPENRB_DEBUG(
                 "A different USB MIDI Device is already connected.\r\nOnly one device at a time is "
@@ -215,8 +193,10 @@ void tuh_midi_mount_cb(uint8_t dev_addr, uint8_t in_ep, uint8_t out_ep, uint8_t 
 
 // Invoked when device with hid interface is un-mounted
 void tuh_midi_umount_cb(uint8_t dev_addr, uint8_t instance) {
-    if (dev_addr == midi_dev_addr) {
-        midi_dev_addr = 0;
+    (void)instance;
+
+    if (dev_addr == drum_state.midi_dev_addr) {
+        drum_state.midi_dev_addr = 0;
         OPENRB_DEBUG("MIDI device address = %d, instance = %d is unmounted\r\n", dev_addr,
                      instance);
     } else {
@@ -225,4 +205,7 @@ void tuh_midi_umount_cb(uint8_t dev_addr, uint8_t instance) {
     }
 }
 
-void tuh_midi_rx_cb(uint8_t dev_addr, uint32_t num_packets) {}
+void tuh_midi_rx_cb(uint8_t dev_addr, uint32_t num_packets) {
+    (void)dev_addr;
+    (void)num_packets;
+}
